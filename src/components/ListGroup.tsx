e12c7aebe5863a656bec4f642303e9c4270bae65
Rath Samrith <rath.samrith@axitvit.com>
2024-11-04 16:47:19 +0700

    f03(managing-state): add stat management for highlighting

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,33 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 function ListGroup() {
   const cities = ["New York", "LA", "Tokyo"];
 
   /**
-   * @function handleClick is an event handler
-   * @param e 
-   * @returns 
+   * 
+   * @useState is a React Hook that lets you add a state variable to your component. 
+   * const [state, setState] = useState(initialState) useState(initialState) set functions, like 
+   * setSomething(nextState)
    */
-  const handleClick = (e: MouseEvent) => console.log("clicked", e);
+  // hook
+  const [selecedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1 className="text-primary text-center">List Group</h1>
       <div className="container">
         <ul className="list-group">
-          {cities.map((item) => (
-            <li className="list-group-item" key={item} onClick={handleClick}>
+          {cities.map((item, index) => (
+            <li
+              className={
+                selecedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item "
+              }
+              key={item}
+              onClick={() => {
+                setSelectedIndex(index);
+              }}
+            >
               {item}
             </li>
           ))}
