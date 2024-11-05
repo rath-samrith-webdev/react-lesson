60bbfca39f33e24f603f1d0234f36bd3bf92dd7d
Rath Samrith <rath.samrith@axitvit.com>
2024-11-05 09:10:43 +0700

    f03(prop-data): allow data tobe passed down to components using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import ListGroup from "./components/ListGroup";
+import ListGroupFTProps from "./components/ListGroupFTProps";
 function App() {
+  const cities=['Phnom Penh','LA','Tokyo','Paris','Jakata']
   return (
     <div className="container">
       <ListGroup></ListGroup>
+      <ListGroupFTProps items={cities}/>
     </div>
   );
 }
