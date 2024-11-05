adbca5059523eb5c03641d0686797d39a18b555f
Rath Samrith <rath.samrith@axitvit.com>
2024-11-05 10:59:25 +0700

    f05(passing-children): allow passing children using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import ListGroup from "./components/ListGroups";
+import Alert from "./components/Alert";
 
 function App() {
-  const cities = ["New York", "LA", "Tokyo"];
-  /**
-   * @function handleSelectItem takes items with the type string as the
-   * @param
-   * @returns @void while @log @item into the @console
-   */
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <>
-      <ListGroup
-        items={cities}
-        heading="Cities"
-        onSelectedItem={handleSelectItem}
-      />
+      <Alert>
+        <span>Test</span>
+      </Alert>
     </>
   );
 }
