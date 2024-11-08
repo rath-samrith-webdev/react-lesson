b02fbbd017e7d8044edb74ee08504894cdd47167
Rath Samrith <rath.samrith@axitvit.com>
2024-11-08 16:06:37 +0700

    (state/component-state-ss-1):pure and impure components

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,3 +1,22 @@
+
+/**
+ * @Impure example
+ * * This here will return different result meaning the component will
+ * * change content when being rendered.
+ * * let count = 0;
+ * * const Message = () => {
+ * *    count++;
+ * *    return <div>Count {count}</div>;
+ * * }
+ * @returns 
+ */
+
+/**
+ * @Pure example 
+ * * Pure meaning that the component returns the same value no matter how 
+ * * many time you render it.
+ * @returns 
+ */
 const Message = () => {
   let count = 0;
   count++;
