f1be571f6b01f18b47def15d507f3f3169984f98
Rath Samrith <rath.samrith@axitvit.com>
2024-11-08 16:23:20 +0700

    state(component-state-ss-2): update object

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import ListGroup from "./components/ListGroup";
+import UpdateObj from "./components/UpdatingObject";
 
 function App() {
   const items = ["Test", "Test1"];
   return (
     <>
+        <UpdateObj/>
       <ListGroup items={items} />
     </>
   );
