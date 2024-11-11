42740c09c66f557223ff1bac1e012d4ef5c40cf8
Rath Samrith <rath.samrith@axitvit.com>
2024-11-11 16:39:42 +0700

    form(manage-form-with-react-hook-form): use react-hook-form library to manage forms

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,23 @@
-import {FormEvent, useState} from "react";
+import {FieldValues, useForm} from "react-hook-form";
 
 const Form = () => {
-  const [person,setPerson] = useState({
-      name: "",
-      age:''
-  });
-
-  const handleSummit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log("submitted person: =>", person);
-  };
+  const {register,handleSubmit}=useForm()
+    const onSubmit=(data:FieldValues)=>{
+      console.log(data)
+    }
   return (
-    <form onSubmit={handleSummit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input onChange={(event)=>setPerson({...person,name:event.target.value})}
-               value={person.name} id="name" type="text" className="form-control" />
+        <input {...register('name')} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input onChange={(event)=>setPerson({...person,age:event.target.value})} value={person.age} type="number" id="age" className="form-control" />
+        <input {...register('age')}  type="number" id="age" className="form-control" />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
