53e83ef0f8393f950180d6f804e5404a07202f5e
Rath Samrith <rath.samrith@axitvit.com>
2024-11-27 12:59:53 +0700

    backend-connection(session-1): understanding useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Form from "./Form";
+import Form from "./Form.tsx";
 
 function App() {
   return (
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,34 +1,22 @@
-import { FormEvent, useRef } from "react";
+import {useEffect, useRef} from "react";
 
-const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: "" };
-
-  const handleSummit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = ageRef.current.value;
-    console.log("submitted person: =>", person);
-  };
-  return (
-    <form onSubmit={handleSummit}>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
-        </label>
-        <input ref={ageRef} type="number" id="age" className="form-control" />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
-  );
-};
+/**
+ * @useEffect is a function is usually called after the rendering of each components to prevents the sides affects and make components
+ * remains pure
+ **/
+const Form=()=>{
+    const refer = useRef<HTMLFormElement>(null);
+    useEffect(()=>{
+        if(refer.current) refer.current.focus()
+    })
+    useEffect(()=>{
+        document.title = 'useEffect';
+    })
+    return (<>
+        <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input ref={refer} id="email" type="email"/>
+        </div>
+    </>)
+}
 export default Form;
