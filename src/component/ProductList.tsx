45747be57c84ef82077ab0fe10651d52990bc9d4
Rath Samrith <rath.samrith@axitvit.com>
2024-11-27 13:23:30 +0700

    backend-connection(session3): clean up effect

--- src/component/ProductList.tsx
+++ src/component/ProductList.tsx
@@ -1,18 +1,25 @@
 import { useEffect, useState } from "react";
 
 const ProductList = ({ category }: { category: string }) => {
-  const [products, setProduct] = useState<string[]>([]);
+  const [products] = useState<string[]>([]);
 
   /**
    * @useEffect takes two parametres 1 is the functions which execute after the component render has been completed
-   * 2 is the dependancy 
-   * @Note that the @useEffect with run only once when there's a dependancy if the @dependancy value changed the 
+   * 2 is the dependancy
+   * @Note that the @useEffect with run only once when there's a dependancy if the @dependancy value changed the
    * @useEffect will be re-run again
    */
+
+  const connect = () => {
+    console.log("Now Connecting", products);
+  };
+  const disconnect = () => {
+    console.log("Now Disconnecting", category);
+  };
   useEffect(() => {
-    console.log("Fetching product form", category);
-    setProduct(["clothing", "house holde"]);
-  }, [category]);
+    connect();
+    return () => disconnect();
+  });
   return <div>ProductList</div>;
 };
 export default ProductList;
